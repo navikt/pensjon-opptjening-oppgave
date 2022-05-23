b423e93d1f7f3c16fd0813721232e826b760af16
a150238 <eivind.askestad@nav.no>
2022-05-23 09:27:22 +0200

    Spring boot

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "pensjon-opptjening-oppgave"
---
+++ src/main/kotlin/no/nav/pensjon/opptjening/pensjonopptjeningoppgave/PensjonOpptjeningOppgaveApplication.kt
@@ -0,0 +1,11 @@
+package no.nav.pensjon.opptjening.pensjonopptjeningoppgave
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PensjonOpptjeningOppgaveApplication
+
+fun main(args: Array<String>) {
+    runApplication<PensjonOpptjeningOppgaveApplication>(*args)
+}
